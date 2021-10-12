037d560d26a49cb464f2ef7b59f994d9557952b0
Eyad alOmari <e.alomari@clicks.local>
2021-10-12 11:26:09 +0300

    Building a custom authentication backend

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*z8hto_0m=$n=6fkxn8k7!gfs7z9a#p#ob@hc!v9q5^n1(k&4q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
 ]
 
 MIDDLEWARE = [
